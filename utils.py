f49fe65de3700aca27e7df7c0f93f4db2eb83214
johngao01 <1649868243@qq.com>
2023-05-19 21:27:26 +0800

    更新网站主页

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ urllib3.disable_warnings()
 header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                         '(KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 
-domain = "https://www.xgmn02.com"
+domain = "https://www.xgmn08.com"
 
 
 def get_organization_had_info(organization: Optional[str]):
